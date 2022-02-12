e881e773faf2a533444e7d33310a05d560b74982
Matthew Spence <mspence5555@gmail.com>
2022-02-11 18:12:12 -0600

    added toggle switch in place of button

--- weather-app/src/components/Searchbar.vue
+++ weather-app/src/components/Searchbar.vue
@@ -189,7 +189,13 @@ export default {
             <p id="cityName">{{ cityName }}, {{ state }} {{ country }}</p>
             <img :src="weatherImageUrl" id="weatherImg">
             <p id=currentTemp>{{ currentTemp }}</p>
-            <button @click="convertTemp(this.kelvin)" id="tempUnitBtn">{{ tempBtn }}</button>
+
+            <label class="switch">
+                <input @click="convertTemp(this.kelvin)" type="checkbox">
+                <span class="slider round"></span>
+            </label>
+
+        
         </div>
     </div>
     
@@ -275,4 +281,69 @@ li:hover {
     background-color: #41AEA9;
     color: white;
 }
+/* start button slider switch*/ 
+
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* Button slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ddd;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: #FABB51;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #ddd;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #ddd;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+/* end button slider switch*/ 
 </style>
