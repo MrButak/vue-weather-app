6ca49cbab5afa4c6c81e6f3a00cc2785afd16809
Matthew Spence <mspence5555@gmail.com>
2022-03-31 07:41:36 -0500

    database calls now working

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,32 @@
 const locations = require('../public/javascripts/locations')
 
-exports.searchCity = (req, res, next) => {
+exports.searchCity = async (req, res, next) => {
 
     data = Object.keys(req.body)
     data = JSON.parse(data)
-    let cities = locations.searchByName(data.city, data.country);
-    res.status(200).json(cities)
+
+    try {
+        let cities = await locations.searchByName(data.city, data.country);
+        res.status(200).json(cities);
+    }
+    catch(err) {
+        console.log(err);
+    };
+    
 };
 
-exports.searchCountry = (req, res, next) => {
-    
+exports.searchCountry = async (req, res, next) => {
     
     let country = Object.keys(req.body)
     country = country[0];
-    let countries = locations.searchByCountry(country);
-    res.status(200).json(countries)  
+
+    try {
+        let countries = await locations.searchByCountry(country);
+        res.status(200).json(countries);
+    }
+    catch(err) {
+        console.log(err);
+    };
+     
 };
+
--- public/javascripts/locations.js
+++ public/javascripts/locations.js
@@ -7,12 +7,12 @@ const client = new Client({
         rejectUnauthorized: false
     }
 });
-//
+
 
 client.connect();
 
 // Query DB for city and country
-exports.searchByName = async(cityName, countryName) => {
+exports.searchByName = async (cityName, countryName) => {
 
     // console.log
     const text = 'SELECT * FROM cities WHERE name ILIKE ($1) AND country ILIKE ($2) LIMIT 30';
@@ -20,45 +20,30 @@ exports.searchByName = async(cityName, countryName) => {
 
     
     const res = await client.query(text, values);
+    
+   
     //await client.end();
     return res.rows;
     
-    // let db = new Database('citylist.db', {verbose: console.log});
-
-	// try {
-	// 	let statement = db.prepare('SELECT * FROM cities WHERE name LIKE (?) AND country = (?) LIMIT 30');
-	// 	let city = statement.all(cityName + "%", countryName);
-	// 	db.close();
-	// 	return city;
-	// }
-    // catch (e) {
-	// 	console.log(e);
-	// };
+    
     
 };
 
 // Query DB for countries
-exports.searchByCountry = async(countryName) => {
+exports.searchByCountry = async (countryName) => {
+
 
     const text = 'SELECT DISTINCT country FROM cities WHERE country ILIKE ($1) LIMIT 10';
     const values = [countryName + "%"];
+
     // const text = 'SELECT DISTINCT country FROM cities WHERE country LIKE ($1) LIMIT 10 RETURNING *';
 
-   
+
     const res = await client.query(text, values);
+
     //await client.end();
+    
     return res.rows;
     
     
-    // let db = new Database('citylist.db', {verbose: console.log});
-
-	// try {
-	// 	let statement = db.prepare('SELECT DISTINCT country FROM cities WHERE country LIKE (?) LIMIT 10');
-	// 	let country = statement.all(countryName + "%");
-	// 	db.close();
-	// 	return country;
-	// } 
-    // catch (e) {
-	// 	console.log(e);
-	// };
 };
