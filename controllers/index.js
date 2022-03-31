19e0e04e4f424725927718cd9f9e57a1c57f5dd2
Matthew Spence <mspence5555@gmail.com>
2022-03-31 08:32:18 -0500

    remove try/catch

--- controllers/index.js
+++ controllers/index.js
@@ -5,13 +5,10 @@ exports.searchCity = async (req, res, next) => {
     data = Object.keys(req.body)
     data = JSON.parse(data)
 
-    try {
-        let cities = await locations.searchByName(data.city, data.country);
-        res.status(200).json(cities);
-    }
-    catch(err) {
-        console.log(err);
-    };
+   
+    let cities = await locations.searchByName(data.city, data.country);
+    res.status(200).json(cities);
+   
 };
 
 exports.searchCountry = async (req, res, next) => {
@@ -19,11 +16,8 @@ exports.searchCountry = async (req, res, next) => {
     let country = Object.keys(req.body)
     country = country[0];
 
-    try {
-        let countries = await locations.searchByCountry(country);
-        res.status(200).json(countries);
-    }
-    catch(err) {
-        console.log(err);
-    };
+    
+    let countries = await locations.searchByCountry(country);
+    res.status(200).json(countries);
+    
 };
