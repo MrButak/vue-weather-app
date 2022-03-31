91ffcac1cdb0b899713f07a860931a1bebd9d3fd
Matthew Spence <mspence5555@gmail.com>
2022-03-31 07:49:45 -0500

    removed middleware for dev

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,6 @@ var router = express.Router();
 var app = express();
 const index = require('../controllers/index')
 
-
-// allow requests from vue front end
-const allowRequest = app.use(function(req, res, next) {
-    
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-
 // POST route for city search
 router.post('/searchcity', index.searchCity);
 
